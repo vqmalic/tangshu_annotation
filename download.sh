430068f25962cd4b5e6c31506463cf998d43ad51
Vincent Malic <vqmalic@gmail.com>
2018-01-26 20:46:48 -0500

    initial

---
+++ download.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo 'Downloading book data.'
+wget http://www.qalaymiqan.com/data/tangshu/zhengshi.pkl
+echo 'Downloading CBDB.'
+wget http://www.qalaymiqan.com/data/tangshu/cbdb_sqlite.db
